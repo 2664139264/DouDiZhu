6d80f379c22285de02f47db5bcd69cbf9d2cead2
汪永毅 <50904422+2664139264@users.noreply.github.com>
2021-03-09 19:06:56 +0800

    added calc_init4

--- count_initial.py
+++ count_initial.py
@@ -6,6 +6,12 @@ def gen_alloc3(n):
         for j in range(n-i+1):
             yield (i, j, n-i-j)
 
+def gen_alloc4(n):
+    for i in range(n+1):
+        for j in range(n-i+1):
+            for k in range(n-i-j+1):
+                yield (i, j, k, n-i-j-k)
+                
 def add(tr1, tr2):
     return tuple([i+j for i,j in zip(tr1, tr2)])
 
@@ -22,14 +28,27 @@ def calc_init(card_rep):
     return A
 
 
+def calc_init4(card_rep):
+    card_type = len(card_rep)
+    A = {-1: {(0,0,0,0):1}}
+    for i in range(card_type):
+        A[i] = dict()
+        for alloc_vec in gen_alloc4(card_rep[i]):
+            for k, v in A[i-1].items():
+                new_alloc_vec = add(k, alloc_vec) 
+                A[i][new_alloc_vec] = v + (
+                    A[i][new_alloc_vec] if new_alloc_vec in A[i].keys() else 0)
+    return A
+
 print(calc_init(card_rep)[14][(20,17,17)])
 
 joker0 = (13 * calc_init([1]+card_rep[1:])[14][(17,17,17)] + 
-    156 * calc_init([2, 3]+card_rep[2:])[14][(17,17,17)])
+    156 * calc_init([2, 3]+card_rep[2:])[14][(17,17,17)] + 
+    286 * calc_init([3,3,3]+card_rep[3:])[14][(17,17,17)])
 
 joker1 = (26 * calc_init([2]+card_rep[1:-1])[13][(17,17,17)] + 
     156 * calc_init([3,3]+card_rep[2:-1])[13][(17,17,17)])
 
 joker2 = 13 * calc_init([3]+card_rep[1:-2])[12][(17,17,17)]
 print(joker0+joker1+joker2)
-
+print(calc_init4(card_rep)[14][(3,17,17,17)])
