d6a310fddf4b93e0cdfff1cdcfcee2a825840520
汪永毅 <50904422+2664139264@users.noreply.github.com>
2021-03-16 01:25:20 +0800

    待修改 gen_singles下内容

--- doudizhu_simulate.py
+++ doudizhu_simulate.py
@@ -27,6 +27,15 @@ gen_combs_of = (
     + [gen_quad_with_1_1] # 四带两单
     + [gen_quad_with_2_2] # 四带两对
 )
+
+# 牌组减法
+def minus(base, comb):
+    return [i-j for i,j in zip(base, comb)]
+
+# 牌组加法
+def add(comb1, comb2):
+    return [i+j for i,j in zip(comb1, comb2)]
+
 # 生成1,2,3,4张大小相同的牌
 def gen_1234(n):
     # k=n表示牌的张数(1,2,3,4)
@@ -90,7 +99,7 @@ def gen_seq_123(n):
         # 最多11-card_e步，如果对方连到card[11]: 'A'，则无可前进空间
         max_step = 11 - card_e
         for i in range(0, max_step):
-            # 从card_s+i的后一位开始看是否有可用连牌
+            # 从card_s+i的后一位(card_s+i+1)开始看是否有可用连牌
             for j in range(card_s+i+1, card_e+i+2):
                 if useable_card[j] < k:
                     break
@@ -98,19 +107,64 @@ def gen_seq_123(n):
                 yield [0]*(card_s+i+1) + [k]*(card_e-card_s+1) + [0]*(13-card_e-i)
     return gen_seq_k
 
+# 以下需要根据竞技二打一比赛规则调整：
+# 飞机不能含有炸弹、火箭，也不能带与飞机相连续的三张牌
+
+# 生成三带所用单牌
+def gen_singles(n, useable_card, s=0, record=[0]*15):
+    if n == 0:
+        yield record
+        return
+    if s == 15 or sum(useable_card[s:]) < n:
+        return
+    # s处可用的单牌数
+    useable = min(useable_card[s], n)
+    for i in range(1, useable+1):
+        yield from gen_singles(n-i, useable_card, s+1, record[:s]+[i]+record[s+1:])
+    
+# 生成三带所用对牌
+def gen_pairs(n, useable_card, s=0, record=[0]*15):
+    if n == 0:
+        yield record
+        return
+    if s == 15 or sum(useable_card[s:]) < n*2:
+        return
+    # s处可用的对牌数
+    useable = min(useable_card[s]//2, n)
+    for i in range(1, useable+1):
+        yield from gen_pairs(n-i, useable_card, s+1, record[:s]+[2*i]+record[s+1:])
+    
 # 生成（连1-5）三带一、对
 def gen_tri_12345_with_12(n, m):
     def gen_tri_n_with_m(useable_card, k=n, l=m, base_card=None):
-        if not base_card:
-            
+        # 本家可用的牌
+        sum_useable_card = sum(useable_card)
+        # 上家出待压制的牌
+        sum_base_card = sum(base_card)
+        # 如果牌不够，没法出
+        if base_card and sum_useable_card < sum_base_card:
             return
-        cnt_usable_card = sum(useable_card)
-        cnt_base_card = sum(base_card)
-        if cnt_base_card > cnt_usable_card:
-            return
-        card_s = base_card.index(3)
-        card_e = 14-base_card[::-1].index(3)
-
+        # 如果是三带单、对
+        if k == 1:
+            if sum(useable_card) < 3*k+l:
+                return
+            # 生成三张牌
+            for tri in gen_1234(3)(useable_card=useable_card):
+                left = minus(useable_card, tri)
+                for w in {1:gen_singles, 2:gen_pairs}[l](k, useable_card=left):
+                    yield add(tri, w)
+        # 如果是连三带单、对
+        # 这里会有歧义性：
+        # 比如333444555666 
+        # 可以理解为3336 4446 5556
+        # 也可以理解为 4443 5553 6663 （默认取大的）
+        tri_e = base_card.index(3)
+        # 寻找结束的4
+        try:
+            tri_e = max(tri_e, base_card.index(4))
+        except:
+            pass
+        
     
     return gen_tri_n_with_m
 
@@ -122,50 +176,69 @@ initial_state = {
 }
 
 # 根据需要压制的出牌类型生成能压制其的牌
-def gen_comb_above(base_card=None, useable_card):
-    # 如果是首次出牌或者无人压制再次轮到
-    if not base_card:
+def gen_comb_above(base_card_type=None, useable_card):
+    # 如果是首次出牌或者无人压制再次轮到，不能过牌，只能出
+    if not base_card_type:
         # 尝试所有的可能出牌种类（单、对、三、炸、带、连）
         for k, v in card_type.items():
             # 枚举该出牌类型下的所有可能出牌方式
             for comb in gen_combs_of[v](useable_card=useable_card):
+                # 返回牌组、类型
                 yield comb, card_type[k]
     else:
-        yield [0]*15, None # 可以选择过牌
-        for comb in gen_combs_of[base_card['type']](
-            base_card=base_card['card'], useable_card=useable_card):
-            yield comb, base_card['type']
-        if base_card['type'] != card_type['B']:
+        # 上家出牌后，可以选择过牌，类型为空
+        yield [0]*15, None 
+        # 如果是为了压制上家牌（出同种更大的）
+        for comb in gen_combs_of[base_card_type['type']](
+            base_card_type=base_card_type['card'], useable_card=useable_card):
+            yield comb, base_card_type['type']
+        # 如果上家出的不是炸弹或者火箭，可以尝试出炸弹压制
+        # （如果上家是炸弹，则已经在上一步被压制了）
+        if base_card_type['type'] not in {card_type['B'], card_type['R']}:
             for comb in gen_combs_of[card_type['B']](useable_card=useable_card):
                 yield comb, card_type['B']
-        if base_card['type'] != card_type['R']:
+        # 如果上家出的不是火箭，尝试出火箭压制
+        if base_card_type['type'] != card_type['R']:
             for comb in gen_combs_of[card_type['R']](useable_card=useable_card):
                 yield comb, card_type['R']
-        
-        
-def minus(base, comb):
-    return [i-j for i,j in zip(base, comb)]
 
+# 完美信息牌局模拟
 def play(state):
-    curr = state['turn'] # 当前应该出牌的玩家
-    useable_card = state['card'][curr] # 当前待出牌玩家可用的牌
-    last = state['last']['turn'] # 上一个未被压制的出牌者
-    base_card = state['last']
+    # 当前应该出牌的玩家
+    curr = state['turn']
+    # 当前待出牌玩家可用的牌
+    useable_card = state['card'][curr]
+    # 未被压制的上家
+    last = state['last']['turn'] 
+    # 未被压制的上家牌+上家牌类型
+    base_card_type = state['last']
+    # 未被压制的上家牌
+    base_card = base_card_type['card']
+    # 如果本家上一轮所出的牌无人压制
     if curr == last:
-        base_card = None
+        # 清空待压制牌
+        base_card_type = None
+    # 尝试出牌压制上家（如果没有上家，则遍历所有出牌方式）
     for comb, comb_type in gen_comb_above(
-        base_card=base_card, useable_card=useable_card):
+        base_card_type=base_card_type, useable_card=useable_card):
+        # 输出玩家号（0=地主，1=农民1，2=农民2）
         print(curr, comb)
+        # 状态转移
         next_state = {
+            # 轮到下家出牌
             'turn':(curr+1)%3,
+            # 仅本家牌减少
             'card':(state['card'][:curr] 
-                + minus(state['card'][curr], comb) 
+                + minus(useable_card, comb) 
                 + state['card'][curr+1:]
             ),
+            # 如果本家没有过牌（comb_type不为空），则待下家压制的牌为本家所出，否则待压制牌不变
             'last':({'turn':curr, 'card':comb, 'type':comb_type} 
-                if comb_type else state['last']
+                if comb_type else base_card_type
             )
         }
+        # 如果本家牌清空，本家胜
         if sum(next_state['card'][curr]) == 0:
             return curr
+        # 否则递归模拟所有分支
         play(next_state)
